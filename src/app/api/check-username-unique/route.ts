15193ed007c9c3f75de3bde07f8162327d757c5e
Prakhar_Github <prakhar.5779.aps3@gmail.com>
2024-12-18 19:38:41 +0530

    next auth setup

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,79 @@
+import ConnectDB from "@/lib/ConnectDB";
+import UserModel from "@/model/User.model";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { z } from "zod";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  /* 
+          // TODO: USE THIS IN  ALL OTHER ROUTES
+          if (request.method !== "GET") {
+            return Response.json(
+              { success: false, message: "ONLY GET METHOD IS ALLOWED!" },
+              { status: 405 }
+            );
+          } 
+  */
+
+  await ConnectDB();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    // validate with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    //TODO:remove console.log(result)
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      { status: 400 }
+    );
+  } catch (err) {
+    console.error("Error checking username", err);
+    return Response.json(
+      { success: false, message: "Error checking username" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,56 @@
+import ConnectDB from "@/lib/ConnectDB";
+import UserModel from "@/model/User.model";
+// import { usernameValidation } from "@/schemas/signUpSchema";
+// import { z } from "zod";
+
+export async function PSOT(request: Request) {
+  await ConnectDB();
+
+  try {
+    const { username, code } = await request.json();
+
+    const decodedUsername = decodeURIComponent(username); // decodes the url
+
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User NOT found" },
+        { status: 500 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        { success: true, message: "Account Verified" },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message: "Verification code has expired, please sign-in again",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        { success: false, message: "Incorrect Verification code" },
+        { status: 400 }
+      );
+    }
+  } catch (err) {
+    console.error("Error verifying User", err);
+    return Response.json(
+      { success: false, message: "Error Verifying User" },
+      { status: 500 }
+    );
+  }
+}
